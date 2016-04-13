bc69b60c1a3fdb1af0046b04d66cb345983af540
David Dang <ddang.sw@gmail.com>
2016-04-13 09:01:12 -0700

    Add body-parser.  Add mongoose code to store the site data and handle redirection

--- linktrack.js
+++ linktrack.js
@@ -15,30 +15,111 @@ var wsLink = "/links";
 var express = require("express");
 var app = express();
 
+// Include body parser
+var parser = require("body-parser");
+app.use(parser.json());
+
+// Require hw7 module
+var mongoose = require("mongoose");
+var dbName = "hw7";
+var dbUri = "mongodb://localhost/"  + dbName;
+
+// Define the schema and create a model for it
+var siteSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true},
+    link: String,
+    click: Number,
+});
+// Create the model
+var Site = mongoose.model("Site", siteSchema);
+
+// Make connection to Mongodb
+mongoose.connect(dbUri);
+mongoose.connection.once("open", function () {
+    console.log("Database open successfully for DB:" + dbName);
+});
+
 // Expose GET on /links
 app.get(wsLink, function (request, response) {
     console.log ("Serving GET on " + request.url);
-    response.status(200).end("Serviing GET on " + request.url);
+
+    // Query for all links and exclude the field _id and __v from result
+    var query = {};
+    var exclude = {_id:0, __v:0};
+    Site.find(query).select(exclude).exec(function (err, sites) {
+        if (err) {
+            console.log("Error finding links: " + err);
+            response.status(500).end("Error encountered search");
+        } else {
+            response.status(200).json(sites).end();
+        }
+    });
+
 });
 
 // Expose POST on /links
 app.post(wsLink, function (request, response) {
     console.log("Serving POST on " + request.url);
-    response.status(200).end("Serving POST on " + request.url);
+    if (! request.hasOwnProperty("body")) {
+        console.log("Error: Missing body-parser module or is not configured");
+        response.status(500).end("Missing configured");
+    }
+
+    try {
+        // Initialize a new site to save
+        var site = new Site();
+        site.title = request.body.title;
+        site.link = request.body.link;
+        site.click = 0;
+
+        // save it
+        site.save(function (err) {
+            if (err) {
+                console.log("Error saving site" + err);
+                response.status(500).end("Error post encountered");
+            } else {
+                console.log("Save successful");
+                response.status(200).json({status:"success"}).end();
+            }
+        });
+    } catch (err) {
+        console.log("Error encounter: " + err);
+        response.status(500).end("Error post encountered");
+    }
+
 });
 
 // Expose GET on /click
 app.get(wsClick, function (request, response) {
+    var query;
+    var updateClick = {$inc: {click: 1}};
 
-    var title = request.params.title;
-
-    if (title === "google") {
-        response.redirect("https://google.com");
+    // Retrieve the title clicked
+    if (request.params.title !== "undefine") {
+        query = {title: request.params.title};
     } else {
-        response.redirect("http://yahoo.com");
+        console.log("Missing title");
+        response.status(400).end("Missing title");
     }
 
-    console.log("Serving GET on " + request.url);
+    // Query the database, update the click by 1 then redirect
+    Site.findOneAndUpdate(query, updateClick, function(err, site) {
+        if (err) {
+            console.log("Error trying to increase click: " + err);
+            response.status(500).end("Error encountered");
+        } else if ( site === null) {
+            // Not found
+            console.log("No data for title: " + query.title);
+            response.status(400).end("Invalid request. No such title");
+        } else {
+            // We are good to redirect here
+            response.redirect(site.link);
+        }
+    });
+
 });
 
 // Run the server on the serverPort
